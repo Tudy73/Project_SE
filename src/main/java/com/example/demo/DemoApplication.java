7afc0da6f10b3b7dcf3581f44a3deeda96436d07
tudor baraian <baraiantudor@yahoo.com>
2023-11-14 18:32:31 +0200

    fourth commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,6 +11,8 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+	String str ="string";
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
