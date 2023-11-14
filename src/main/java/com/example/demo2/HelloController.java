17c58d2ef24c7faa036fe78b814ae572d0e6b80d
tudor baraian <baraiantudor@yahoo.com>
2023-11-14 19:08:51 +0200

    11/14/2023 #1

--- src/main/java/com/example/demo2/HelloController.java
+++ src/main/java/com/example/demo2/HelloController.java
@@ -6,13 +6,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
     @GetMapping("/hello")
-public String hello(){
-        Connect q=new Connect("cont");
-        String s=" ";
-for(int i=0;i<q.ultimul_index("hello");i++)
-{
-    s+=q.getName(i)+" ";
-}
-    return s;
-}
+    public String hello() {
+        Connect q = new Connect("cont");
+        StringBuilder returnedString = new StringBuilder(" ");
+        for (int i = 0; i < q.tableSize("hello"); i++) {
+            returnedString.append(q.getName(i)).append(" ");
+        }
+        return returnedString.toString();
+    }
 }
